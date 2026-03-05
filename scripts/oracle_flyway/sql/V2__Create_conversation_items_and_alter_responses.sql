030280c03ad631d5170f9d1517d1133c9db705d8
Keyang Ru <rukeyang@gmail.com>
2026-03-04 21:30:44 -0800

    refactor(oracle): use existing CONVERSATIONS table in Flyway test schema (#627)
    Signed-off-by: Keyang Ru <rukeyang@gmail.com>

--- scripts/oracle_flyway/sql/V2__Create_conversation_items_and_alter_responses.sql
+++ scripts/oracle_flyway/sql/V2__Create_conversation_items_and_alter_responses.sql
@@ -1,28 +1,8 @@
--- V2: Create V2 conversation tables and alter RESPONSES
+-- V2: Create conversation item tables and alter RESPONSES
 -- CI-only version: tables + indexes, no sweep procedures or scheduler jobs.
 
 ------------------------------------------------------------
--- 1. CONVERSATIONS_V2
-------------------------------------------------------------
-CREATE TABLE CONVERSATIONS_V2 (
-    ID                       VARCHAR2(255)              NOT NULL,
-    CONVERSATION_STORE_ID    VARCHAR2(255),
-    GENERATIVE_AI_PROJECT_ID VARCHAR2(255),
-    CREATED_AT               TIMESTAMP WITH TIME ZONE   NOT NULL,
-    METADATA                 CLOB,
-    SHORT_TERM_MEMORY        CLOB,
-    SHORT_TERM_MEMORY_BYTES  NUMBER,
-    UPDATED_AT               TIMESTAMP WITH TIME ZONE,
-    EXPIRES_AT               TIMESTAMP WITH TIME ZONE   DEFAULT (SYSTIMESTAMP + INTERVAL '30' DAY) NOT NULL,
-    CONSTRAINT PK_CONVERSATIONS_V2 PRIMARY KEY (ID)
-);
-
-CREATE INDEX IDX_CONV_V2_EXPIRES_AT ON CONVERSATIONS_V2 (EXPIRES_AT);
-CREATE INDEX IDX_CONV_V2_PROJECT_ID ON CONVERSATIONS_V2 (GENERATIVE_AI_PROJECT_ID);
-CREATE INDEX IDX_CONV_V2_STORE_ID ON CONVERSATIONS_V2 (CONVERSATION_STORE_ID);
-
-------------------------------------------------------------
--- 2. CONVERSATION_ITEMS
+-- 1. CONVERSATION_ITEMS
 ------------------------------------------------------------
 CREATE TABLE CONVERSATION_ITEMS (
     ID                       VARCHAR2(255)              NOT NULL,
@@ -43,7 +23,7 @@ CREATE INDEX IDX_CONV_ITEMS_ITEM_TYPE ON CONVERSATION_ITEMS (ITEM_TYPE);
 CREATE INDEX IDX_CONV_ITEMS_PROJECT_ID ON CONVERSATION_ITEMS (GENERATIVE_AI_PROJECT_ID);
 
 ------------------------------------------------------------
--- 3. CONVERSATION_ITEM_LINKS
+-- 2. CONVERSATION_ITEM_LINKS
 ------------------------------------------------------------
 CREATE TABLE CONVERSATION_ITEM_LINKS (
     CONVERSATION_ID          VARCHAR2(255)              NOT NULL,
@@ -55,6 +35,6 @@ CREATE TABLE CONVERSATION_ITEM_LINKS (
 CREATE INDEX IDX_CONV_ITEM_LINKS_ORDER ON CONVERSATION_ITEM_LINKS (CONVERSATION_ID, ADDED_AT);
 
 ------------------------------------------------------------
--- 4. ALTER RESPONSES: add SAFETY_IDENTIFIER
+-- 3. ALTER RESPONSES: add SAFETY_IDENTIFIER
 ------------------------------------------------------------
 ALTER TABLE RESPONSES ADD (SAFETY_IDENTIFIER VARCHAR2(255));
